34b42d9bdda39306a70b3a62f8eebb752ff4006d
Nagaraj G <nagarajgurusamy20@gmail.com>
2025-08-30 10:01:46 +0530

    Copying the list

--- Exercise/Samples/src/EasyCode.java
+++ Exercise/Samples/src/EasyCode.java
@@ -4,16 +4,28 @@ import java.util.List;
 
 public class EasyCode {
     public static void main(String[] args) {
-        List<String> fruitsList = new ArrayList<>();
+        List<String> fruitsList_1 = new ArrayList<String>();
 
-        fruitsList.add("Apple");
-        fruitsList.add("Banana");
-        fruitsList.add("Pappaya");
-        fruitsList.add("Grapes");
+        fruitsList_1.add("Apple");
+        fruitsList_1.add("Banana");
+        fruitsList_1.add("Pappaya");
+        fruitsList_1.add("Grapes");
+        System.out.println("\n Before Copy: ");
+        System.out.println("fruitsList_1: "+fruitsList_1);
 
-        System.out.println("Before Sorting the List of fruits"+fruitsList);
-        Collections.sort(fruitsList);
-        System.out.println("After Sorting the List of fruits"+ fruitsList);
+        List<String> fruitsList_2 = new ArrayList<String>();
+
+        fruitsList_2.add("Orange");
+        fruitsList_2.add("Dragonfruit");
+        fruitsList_2.add("cherry");
+        fruitsList_2.add("strawberry");
+        System.out.println("fruitList_2: "+fruitsList_2);
+
+        Collections.copy(fruitsList_1,fruitsList_2);
+        System.out.println("\n After Copy: ");
+
+        System.out.println("fruitsList_1: "+ fruitsList_1);
+        System.out.println("fruitsList_2: "+ fruitsList_2);
 
     }
 }
